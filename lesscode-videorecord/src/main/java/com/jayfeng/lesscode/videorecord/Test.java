6d23cab6487764e8c78b1c7540ab25215106fdd0
fengjian <fengjian@lez.cn>
2016-11-28 11:00:19 +0800

    init project.

---
+++ lesscode-videorecord/src/main/java/com/jayfeng/lesscode/videorecord/Test.java
@@ -0,0 +1,13 @@
+package com.jayfeng.lesscode.videorecord;
+
+import android.util.Log;
+
+public class Test {
+
+    /**
+     * test
+     */
+    private void hello() {
+        Log.d("TEST", "world");
+    }
+}
